0280775a98931a9d8c8d0149845cbcead0c092e6
EvanHuang <evan.huang@oocl.com>
2019-06-19 11:29:32 +0800

    基于内存的认证信息
    https://mp.weixin.qq.com/s?__biz=MzA4ODIyMzEwMg==&mid=2447533777&idx=1&sn=419777fe4cd530c67e43cc1e56ccf011&chksm=843bbac0b34c33d63dbbd1cb993bf9fcf03983248d6955ad2dcf275ebaaa046882361c2c53fc&scene=21#wechat_redirect

--- README.md
+++ README.md
@@ -2,6 +2,30 @@
 Spring 5.x 之后， 如果想关闭security校验，#security.basic.enabled=false 是无效的， 使用
 @SpringBootApplication(exclude= SecurityAutoConfiguration.class)
 
+#### 2.基于内存的认证信息
+
+在Spring 5.x之后， 如果添加密码加密方式， 将会报如下错误
+> java.lang.IllegalArgumentException: There is no PasswordEncoder mapped for the id "null"
+
+具体做法：
+``` java
+configure(AuthenticationManagerBuilder auth) {
+    auth.inMemoryAuthentication()
+                    .passwordEncoder(new BCryptPasswordEncoder())  //指定加密方式
+                    .withUser("user")
+                    .password(new BCryptPasswordEncoder().encode("123456")) // 指定加密方式
+                    .roles();
+}
+
+...
+
+@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+```
+
 
 
 
---
+++ src/main/java/com/evan/config/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.evan.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration  // 注解这是一个配置类。
+@EnableWebSecurity  // 注解开启Spring Security的功能。
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        /*
+         * 配置为从内存中进行加载认证信息.
+         * 这里配置了两个用户 admin和user
+         */
+        auth.inMemoryAuthentication()
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("admin")
+                .password(new BCryptPasswordEncoder().encode("123456"))
+                .roles();
+        auth.inMemoryAuthentication()
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("user")
+                .password(new BCryptPasswordEncoder().encode("123456"))
+                .roles();
+    }
+
+    // 注入PsswordEncoder,
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
